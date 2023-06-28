a78bb338382157fd75b2916b51c5580286f3f4d1
Isael Terrones <i.terayo@gmail.com>
2023-06-28 02:11:41 -0600

    estilado

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import LuckyCookie from './components/cookie'
+
+function App() {
+
+  return (
+    <>
+      <LuckyCookie/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/cookie.jsx
@@ -0,0 +1,12 @@
+const LuckyCookie = () => {
+    return(
+        <article className="lucky-cookie">
+            <p>frase de la suerte</p>
+            <div>
+            <button> <i className='bx bx-shuffle bx-sm'></i></button>
+            </div>
+        </article>
+    )
+}
+
+export default LuckyCookie
